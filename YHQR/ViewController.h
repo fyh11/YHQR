ee3945f930bd6bacbdc5fdbcc2d1453592ad66be
Your Name <you@example.com>
2017-05-15 09:43:18 +0800

    xxxxxx

---
+++ YHQR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHQR
+//
+//  Created by 樊义红 on 17/5/15.
+//  Copyright © 2017年 樊义红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YHQR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YHQR
+//
+//  Created by 樊义红 on 17/5/15.
+//  Copyright © 2017年 樊义红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
